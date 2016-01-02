d26e0b18f31de07e76bac7618419e4251d56cfc0
Emeliov Dmitri <demelev@bullyentertainment.com>
2016-01-02 20:25:36 +0200

    Add hook for AssetsController

--- lib/hooks/view_layouts_base_html_head_hook.rb
+++ lib/hooks/view_layouts_base_html_head_hook.rb
@@ -8,7 +8,8 @@ module RedmineLightbox2
                                       context[:controller].is_a?(DocumentsController) ||
                                       context[:controller].is_a?(FilesController) ||
                                       context[:controller].is_a?(MessagesController) ||
-                                      context[:controller].is_a?(NewsController))
+                                      context[:controller].is_a?(NewsController)
+                                      context[:controller].is_a?(AssetsController))
           return stylesheet_link_tag("jquery.fancybox-2.1.5.css", :plugin => "redmine_lightbox2", :media => "screen") +
 			stylesheet_link_tag("lightbox.css", :plugin => "redmine_lightbox2", :media => "screen") +
             javascript_include_tag('jquery.fancybox-2.1.5.pack.js', :plugin => 'redmine_lightbox2') +
